90e0f0fe45745bd2be736f2f6fbf366383881ba1
Luc Ostman <peolina@live.com.au>
2023-05-30 10:00:19 +1000

    resubmitted code for post request in app.post in server.js file

--- server.js
+++ server.js
@@ -13,11 +13,33 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Handle POST request to '/api/chatgpt'
 app.post('/api/chatgpt', (req, res) => {
-  // ... (remaining code for handling the ChatGPT API call)
+  const message = req.body.message;
 
-  // Send the chatbot's response
-  res.json({ message: botReply });
+  // Make API call to ChatGPT
+  fetch('https://api.openai.com/v1/chat/completions', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer YOUR_API_KEY', // Replace with your ChatGPT API key
+    },
+    body: JSON.stringify({
+      messages: [
+        { role: 'system', content: 'You are a helpful assistant.' },
+        { role: 'user', content: message },
+      ],
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const botReply = data.choices[0].message.content;
+      res.json({ message: botReply });
+    })
+    .catch((error) => {
+      console.log('Error:', error);
+      res.status(500).json({ error: 'An error occurred' });
+    });
 });
+
 
 // Serve index.html as the starting page
 app.get('/', (req, res) => {
